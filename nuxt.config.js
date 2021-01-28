00d8e7fea0dbb1a9ad1ed2534a30e34106e4c2e0
desri <deryaprianto4@gmail.com>
2021-01-28 16:49:53 +0700

    update base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
       local: {
         endpoints: {
           login: {
-            url: '/api/jwt-auth/v1/token',
+            url: '/wp-json/jwt-auth/v1/token',
             method: 'post',
             propertyName: 'token'
           },
@@ -68,14 +68,14 @@ export default {
   */
   axios: {
     //baseURL: 'https://news-rest.telunjukatas.tech/wp-json',
-    proxy:true,
+    proxy:false,
     proxyHeaders: false,
     credentials: false,
   },
   
-  proxy: {
-    '/api/': { target: 'https://wordpress-development.indochat.co.id/wp-json/', pathRewrite: {'^/api/': ''},changeOrigin: true }
-  },
+  // proxy: {
+  //   '/api/': { target: 'https://wordpress-development.indochat.co.id/wp-json/', pathRewrite: {'^/api/': ''},changeOrigin: true }
+  // },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
--- store/artikel/actions.js
+++ store/artikel/actions.js
@@ -1,7 +1,7 @@
 export default {
   async fetchArtikels({ commit }) {
     try {
-      const response = await this.$axios.$get(`api/wp/v2/posts?_embed`)
+      const response = await this.$axios.$get(`/wp-json/wp/v2/posts?_embed`)
       commit('SET_ARTIKELS', response)
       //console.log(response[0]._embedded.["wp:term"][0][0].name)
     } catch (error) {
@@ -10,7 +10,7 @@ export default {
   },
   async fetchDetailArtikel({ commit }, slug) {
     try {
-      const response = await this.$axios.$get(`api/wp/v2/posts?slug=${slug}&_embed`)
+      const response = await this.$axios.$get(`/wp-json/wp/v2/posts?slug=${slug}&_embed`)
       commit('SET_DETAIL', response[0])
     } catch (error) {
       throw error
@@ -18,7 +18,7 @@ export default {
   },
   async fetchCategory({ commit }, slug) {
     try {
-      const response = await this.$axios.$get(`api/wp/v2/posts?categories=${slug}&_embed`)
+      const response = await this.$axios.$get(`/wp-json/wp/v2/posts?categories=${slug}&_embed`)
       commit('SET_CATEGORY', response)
     } catch (error) {
       throw error
